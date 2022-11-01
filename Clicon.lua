8b8552a9e17ef9dfa3663f73f4b892b89c1e16e7
Joel C <joel@vorce.se>
2022-11-01 17:47:06 +0100

    Create project and first working version

---
+++ Clicon.lua
@@ -0,0 +1,24 @@
+function SetDefaultPortrait(frame)
+    SetPortraitTexture(frame.portrait, frame.unit)
+    frame.portrait:SetTexCoord(0, 1, 0, 1)
+end
+
+function SetClassPortrait(frame)
+    local _, unitClass = UnitClass(frame.unit)
+    if (unitClass and UnitIsPlayer(frame.unit)) then
+        frame.portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
+        frame.portrait:SetTexCoord(unpack(CLASS_ICON_TCOORDS[unitClass]))
+    else
+        frame.portrait:SetTexCoord(0, 1, 0, 1)
+    end
+end
+
+local SupportedUnits = { ['player'] = true, ['party1'] = true, ['party2'] = true, ['party3'] = true, ['party4'] = true, ['focus'] = true, ['target'] = true }
+
+hooksecurefunc("UnitFramePortrait_Update", function (self)
+    if (SupportedUnits[self.unit] and self.portrait) then
+        SetClassPortrait(self)
+    else
+        SetDefaultPortrait(self)
+    end
+end)
---
+++ README.md
@@ -0,0 +1,4 @@
+# Clicon
+
+World of Warcraft Addon that will display class icon instead of portraits on the unit frames.
+
